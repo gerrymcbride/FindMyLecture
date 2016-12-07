b65005bb69bec5c61c2077914cf4100c11383ebf
Gerard Anthony Mc Bride <GERARD.MCBRIDE.2011@mumail.ie>
2016-12-07 18:13:26 +0000

    Initial commit

---
+++ app/src/main/java/com/example/p170086/findmylecture/On_Loading_Page.java
@@ -0,0 +1,13 @@
+package com.example.p170086.findmylecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class On_Loading_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on__loading__page);
+    }
+}
