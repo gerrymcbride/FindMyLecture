97f209bf83539023e0576de916ae03ef24dbc6bd
Gerard Anthony Mc Bride <GERARD.MCBRIDE.2011@mumail.ie>
2017-01-06 17:28:37 +0000

    Update

--- app/src/main/java/com/example/p170086/findmylecture/MapDirections.java
+++ app/src/main/java/com/example/p170086/findmylecture/MapDirections.java
@@ -4,12 +4,16 @@ package com.example.p170086.findmylecture;
  * Created by gerard on 31/12/16.
  */
 import android.app.Dialog;
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -32,7 +36,9 @@ public class MapDirections extends FragmentActivity implements OnMapReadyCallbac
 
     GoogleMap mGoogleMap;
     Button go;
-
+    String[] places ={"John Hume Building","Eolas Building","Iontas","SQL","JDBC","Web services"};
+    AutoCompleteTextView text;
+    MultiAutoCompleteTextView text1;
 
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
@@ -44,9 +50,14 @@ public class MapDirections extends FragmentActivity implements OnMapReadyCallbac
             setContentView(R.layout.mapresult);
             initMap();
 
+            //text=(AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+            //text1=(MultiAutoCompleteTextView)findViewById(R.id.multiAutoCompleteTextView1);
+
         } else {
             setContentView(R.layout.error);
         }
+
+
     }
 
     private void initMap() {
@@ -103,9 +114,25 @@ public class MapDirections extends FragmentActivity implements OnMapReadyCallbac
     }
 
     public void geoLocate(View view) throws IOException {
+        call();
 
-        EditText et = (EditText)findViewById(R.id.eT);
+
+    }
+
+    public void call()throws IOException{
+        MultiAutoCompleteTextView et = (MultiAutoCompleteTextView)findViewById(R.id.eT);
+        MultiAutoCompleteTextView TT = (MultiAutoCompleteTextView)findViewById(R.id.TT);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, places);
+        et.setAdapter(adapter);
+        et.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+        TT.setAdapter(adapter);
+        TT.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+
         String location = et.getText().toString();
+        String location2 = TT.getText().toString();
 
         Geocoder gc = new Geocoder(this);
         List<Address> list = gc.getFromLocationName(location,1);
@@ -116,13 +143,65 @@ public class MapDirections extends FragmentActivity implements OnMapReadyCallbac
 
         double lat =  address.getLatitude();
         double lon = address.getLongitude();
+
         goToLocationZoom(lat, lon, 15);
 
         MarkerOptions mk = new MarkerOptions()
                 .title(locality)
                 .position(new LatLng(lat, lon));
 
+
+
+        Thread timer = new Thread() {
+            public void run() {
+                /**
+                 * try to sleep() for time
+                 */
+                try {
+
+                    sleep(3000);
+                    /**
+                     * if interrupted catch exception as e
+                     */
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                    /**
+                     * upon completion of trial without occurrence of e
+                     */
+                } finally {
+                    /**
+                     * @param homePage is Intent leading to Menu page
+                     */
+
+
+                }
+            }
+        };
+
         mGoogleMap.addMarker(mk);
+
+        timer.start();
+
+        if(location2.length()!= 0) {
+
+            List<Address> list2 = gc.getFromLocationName(location2,1);
+            Address address2 = list2.get(0);
+            String locality2 = address2.getLocality();
+
+            double lat1 =  address2.getLatitude();
+            double lon1 = address2.getLongitude();
+
+            MarkerOptions mk1 = new MarkerOptions()
+                    .title(locality2)
+                    .position(new LatLng(lat1, lon1));
+
+            mGoogleMap.addMarker(mk1);
+
+        } else {
+            Toast.makeText(this, "Hey! Tell me where you want to go!", Toast.LENGTH_LONG).show();
+        }
+
 
     }
 }
